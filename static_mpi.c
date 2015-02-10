a03b53e8d66e3fd67cc0faddd19df672bd4c6522
wrbeasl <wrbeasl@g.clemson.edu>
2015-02-09 20:15:27 -0500

    Finished

--- static_mpi.c
+++ static_mpi.c
@@ -14,47 +14,30 @@
 #include <unistd.h>
 #include <sys/types.h>
 
-#define MAX_LOAD 1024
-
-/*
-	References:
-	Type 0: .1 - 3.0 Seconds
-	Type 1: 2.0 - 5.0
-	Type 2: 1.0 6.0
-	Type 3: 5.0 7.5
-	Type 4: 7.0 9.0
-*/
-
-/** 
-
-	BASE THE VALUES TO EACH PROCESS BY THE MAX TIMES
-
-
-**/
-
 int main(int argc, char **argv){
 
-	int queue[MAX_LOAD];
+	int queue[1024];
 	int rank, size;
 	int i, j;
 	float maxtime, perTime;
-	int types[5];
+	int Types[5];
 	float timeMax[5] = {3.0, 5.0, 6.0, 7.5, 9.0};
 	float *times;
 	
-	double start, end;
+	clock_t start, end;
 	
 	srand(time(NULL));
 	
 	
 	MPI_Init(&argc, &argv);
-	start = MPI_Wtime();
+	start = clock();
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Status status;
 	
-	int counts[size-1];
-	int disp[size-1];
+	int workCount[size-1];
+	int Proc_Count[size-1];
+	int Proc_Displacement[size-1];
 	
 	float workSleep[size - 1];
 	float workSleepAvg[size - 1];
@@ -62,34 +45,34 @@ int main(int argc, char **argv){
 	float typeSleepAvg[5];
 	
 	for(i = 0; i < size-1; i++){
-		counts[i] = 0;
-		disp[i] = 0;
+		Proc_Count[i] = 0;
+		Proc_Displacement[i] = 0;
 	}
 
 	if(rank == 0){
 		int i = 0;
-		for(i = 0; i < MAX_LOAD; ++i){
+		for(i = 0; i < 1024; ++i){
 			queue[i] = rand() % 5;
 			switch(queue[i]){
 				case 0:
 					maxtime+=timeMax[0];
-					types[0]++;
+					Types[0]++;
 					break;
 				case 1:
 					maxtime+=timeMax[1];
-					types[1]++;
+					Types[1]++;
 					break;
 				case 2:
 					maxtime+=timeMax[2];
-					types[2]++;
+					Types[2]++;
 					break;
 				case 3:
 					maxtime+=timeMax[3];
-					types[3]++;
+					Types[3]++;
 					break;
 				case 4:
 					maxtime+=timeMax[4];
-					types[4]++;
+					Types[4]++;
 					break;
 			}
 		}
@@ -103,63 +86,76 @@ int main(int argc, char **argv){
 		int total = 0;
 		for(i = 1; i < size; i++){
 			time = 0;
-			disp[i-1] = j;
-			while(time < perTime && j < MAX_LOAD){
+			Proc_Displacement[i-1] = j;
+			while(time < perTime && j < 1024){
 				time += timeMax[queue[j]];
 				j++;
-				counts[i-1]++;
+				Proc_Count[i-1]++;
 			}
 		}
 		
 		//this is where we begin setting up the sending process to the slave processes. I haven't figured a way to do this, but I think a 'for' loop might work. I'm not sure how arrays are sent beyond using scatterv, gatherv
 		//What about a for loop that sends based on the process number and sends a preliminary ID-start code and a # of processes code?
 		for(i = 1; i < size; i++){
-			MPI_Send(&counts[i-1], 1, MPI_INT, i, 2, MPI_COMM_WORLD);
-			MPI_Send(&disp[i-1], 1, MPI_INT, i, 3, MPI_COMM_WORLD);
+			MPI_Send(&Proc_Count[i-1], 1, MPI_INT, i, 2, MPI_COMM_WORLD);
+			MPI_Send(&Proc_Displacement[i-1], 1, MPI_INT, i, 3, MPI_COMM_WORLD);
 		}
 		
 		for(i = 1; i < size; i++){
-			MPI_Send(&queue[disp[i-1]], counts[i-1], MPI_INT, i, 4, MPI_COMM_WORLD);
+			MPI_Send(&queue[Proc_Displacement[i-1]], Proc_Count[i-1], MPI_INT, i, 4, MPI_COMM_WORLD);
+			workCount[i-1] = Proc_Count[i-1];
+			//printf("%d\n", workCount[i-1]);
 		}
 		
 		float tempSleep;
 		
-		for(i = 0; i < MAX_LOAD; i++){
+		for(i = 0; i < 1024; i++){
 			MPI_Recv(&tempSleep, 1, MPI_FLOAT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 			typeSleep[queue[status.MPI_TAG]] += tempSleep;
 			workSleep[status.MPI_SOURCE-1] += tempSleep;
 		}
 		
 		for(i = 0; i < 5; i++){
-			typeSleepAvg[i] = typeSleep[i]/types[i];
+			typeSleepAvg[i] = typeSleep[i]/Types[i];
 			printf("Total time slept for type %d is %f\n", i, typeSleep[i]);
-			printf("Average time slept for type %d is %f\n", i, typeSleepAvg[i]);
+			printf("Average time slept for type %d is %f\n\n", i, typeSleepAvg[i]);
 		}
 		
 		int ID;
 		
 		for(i = 0; i < size-1; i++){
 			ID = i + 1;
-			workSleepAvg[i] = workSleep[i]/counts[i];
+			workSleepAvg[i] = workSleep[i]/Proc_Count[i];
 			printf("Total time slept for process %d is %f\n", ID, workSleep[i]);
-			printf("Average time slept for process %d is %f\n", ID, workSleepAvg[i]);
+			printf("Average time slept for process %d is %f\n\n", ID, workSleepAvg[i]);
 		}
+
+		for(i = 1; i < size; ++i){
+			printf("Total number of workloads for process %d : %d\n", i, workCount[i-1]);
+			printf("\n");
+		}
+
+		for(i = 0; i < 5; ++i){
+			printf("Number of workloads for Type %d : %d\n\n", i, Types[i]);
+		}
+
+
 	}
 	
 	else{
-		//This is subprocessor work area. This will need to be sent using a object of some sort, maybe. Could use tags 2 and 3 to send times and ID codes?
-		int numElem, startPoint;
-		MPI_Recv(&numElem, 1, MPI_INT, 0, 2, MPI_COMM_WORLD, &status);
-		MPI_Recv(&startPoint, 1, MPI_INT, 0, 3, MPI_COMM_WORLD, &status);
+		// The working processes area
+		int Number_Of_Elements, Starting_Point;
+		MPI_Recv(&Number_Of_Elements, 1, MPI_INT, 0, 2, MPI_COMM_WORLD, &status);
+		MPI_Recv(&Starting_Point, 1, MPI_INT, 0, 3, MPI_COMM_WORLD, &status);
 		
-		int jobs[numElem];
+		int jobs[Number_Of_Elements];
 		int tag;
 		
-		MPI_Recv(&jobs, numElem, MPI_INT, 0, 4, MPI_COMM_WORLD, &status);
+		MPI_Recv(&jobs, Number_Of_Elements, MPI_INT, 0, 4, MPI_COMM_WORLD, &status);
 		
 		float sleepTime;
 		
-		for(i = 0; i < numElem; ++i){
+		for(i = 0; i < Number_Of_Elements; ++i){
 			sleepTime = ((float)rand()/(float)(RAND_MAX));
 			switch(jobs[i]){
 				case 0:
@@ -184,7 +180,7 @@ int main(int argc, char **argv){
 			}
 			
 			usleep((useconds_t) (sleepTime * 1000));
-			tag = startPoint + i;
+			tag = Starting_Point + i;
 			MPI_Send(&sleepTime, 1, MPI_FLOAT, 0, tag, MPI_COMM_WORLD);
 			}
 	}
